b03cc0d5940b4bfb6f6055e7b7b714a75c852a0c
hyesungHan <comet0910@naver.com>
2022-05-02 17:45:43 +0900

    section bottom area modify

--- assets/js/scroll_bus.js
+++ assets/js/scroll_bus.js
@@ -1,8 +1,37 @@
+// min-width:744px max-width:1024px;
+let scrollArea = window.innerWidth;
+if (scrollArea >= 744) {
+    $('.feature-list li').click(function (){
+        let myBusStop = $(this).index();
+        console.log(myBusStop);
+        $('.bus-content').hide();
+        $('.bus-content').eq(myBusStop).show();
+    });
+} else {
+    $('.bus-content').show();
+}
+
+
+$(window).on('resize', function (){
+    let scrollWidth = window.innerWidth;
+    if (scrollWidth >= 744) {
+        location.reload();
+        $('.feature-list li').click(function (){
+            let myBusStop = $(this).index();
+            console.log(myBusStop);
+            $('.bus-content').hide();
+            $('.bus-content').eq(myBusStop).show();
+        });
+    } else {
+        location.reload();
+    };
+})
+
+
 let href = $(this).attr('href');
 $('.bus-middle-box a').click(function(){
     console.log($(href).offset().top);
 })
-
 
 $(window).scroll(function(){
     let stNum = $(this).scrollTop();
@@ -14,10 +43,8 @@ $(window).scroll(function(){
 
         if(divTop <= stNum) {
             contentNum = $(this).index();
-
             $('.feature-list li').removeClass('is-active');
             $('.feature-list li').eq(contentNum).addClass('is-active');
-            console.log(contentNum);
         }
     })
 })
