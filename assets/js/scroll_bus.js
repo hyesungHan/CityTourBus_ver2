b070193f9a0d373696aaeaee4d690412c9319c94
hyesungHan <comet0910@naver.com>
2022-05-05 17:23:15 +0900

    contents bottom mobile modify

--- assets/js/scroll_bus.js
+++ assets/js/scroll_bus.js
@@ -8,8 +8,36 @@ if (scrollArea >= 744) {
         $('.bus-content').eq(myBusStop).show();
     });
 } else {
-    $('.bus-content').show();
+    let feature1 = $('#feature1').position();
+    let feature2 = $('#feature2').position();
+    let feature3 = $('#feature3').position();
+    let feature4 = $('#feature4').position();
 
+    
+    $('#link1').click(function (){
+        $('html, body').animate({
+            scrollTop:feature1.top
+        }, 500);
+        return false;
+    });
+    $('#link2').click(function (){
+        $('html, body').animate({
+            scrollTop:feature2.top
+        }, 500);
+        return false;
+    });
+    $('#link3').click(function (){
+        $('html, body').animate({
+            scrollTop:feature3.top
+        }, 500);
+        return false;
+    });
+    $('#link4').click(function (){
+        $('html, body').animate({
+            scrollTop:feature4.top
+        }, 500);
+        return false;
+    });
 }
 
 
@@ -29,10 +57,10 @@ $(window).on('resize', function (){
 })
 
 
-let href = $(this).attr('href');
-$('.bus-middle-box a').click(function(){
-    console.log($(href).offset().top);
-})
+// let href = $(this).attr('href');
+// $('.bus-middle-box a').click(function(){
+//     console.log($(href).offset().top);
+// })
 
 
 $(window).scroll(function(){
